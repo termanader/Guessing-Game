2529505d96200530028a85ee0e272f8ea4503f9b
Termanader <austin.nader@gmail.com>
2019-02-09 01:16:23 -0600

    Completed working version

--- GuessingGame.py
+++ GuessingGame.py
@@ -1,4 +1,5 @@
 import random
+import math
 from random import randint
 
 #Generate a random number to begin the game
@@ -6,32 +7,23 @@ x = randint(1,100)
 print(x)
 
 #Placeholder global win condition for game loop
-w = False
+
 
 #Start game loop
-  
-while w == False:
-    try: 
-        p = input ("Guess my number between One and One-Hundred:")
-        g = int(p)+0
-    except ValueError: 
-        print ("Eat a dick, pick an integer between 1 and 100") 
+while True:  
+    while True:
+        try:
+            g = int(input('Guess my number between One and One-Hundred:'))
+            break
+        except:
+            print("An integer greater than 0 and less than one-hundred please...")
+
     v = ""
-
-    if str(p) == "q" or "quit": 
-        w = True
-
-    g = int(p)
-
     if g == x: 
         print("You chose wisely, you WON!!!") 
-        w = True
         break
     if g < x  : v = "You are lower." 
     if g > x  : v = "You are higher." 
     if g > 100: v = "Under 101 please..."
     if g < 1  : v = "Above 0 please..."
     print(v)
-    continue
-    
-    
